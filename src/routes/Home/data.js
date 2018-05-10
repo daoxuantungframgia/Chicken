e4c203e58b3a4cb836901248a3e83b2f62c944c3
HON-DBS-LT21561\Administrator <daotungdtvt>
2018-05-10 14:45:05 +0700

    change icon homepage

--- src/routes/Home/data.js
+++ src/routes/Home/data.js
@@ -3,32 +3,31 @@ import categoryImage1 from '../../assets/ga-mia-3-11_46.jpg'
 import categoryImage2 from '../../assets/ga-mia-ong-tam_38.jpg'
 import categoryImage3 from '../../assets/trung-ga-mia-ngon_50.jpg'
 import categoryImage4 from '../../assets/giong-ga-mia_67.jpg'
-import categoryImage5 from '../../assets/ga-mia-ruou_91.jpg'
 
 export const categories = [
   {
     id: 1,
     image: categoryImage1,
     name: 'Thịt Gà Sạch',
-    active: 'ref1',
+    active: 'ref2',
   },
   {
     id: 2,
     image: categoryImage2,
     name: 'Gà Mía Thả Vườn',
-    active: 'ref2',
+    active: 'ref3',
   },
   {
     id: 3,
     image: categoryImage3,
     name: 'Trứng Gà Sạch',
-    active: 'ref3',
+    active: 'ref4',
   },
   {
     id: 4,
     image: categoryImage4,
     name: 'Gà Giống Con',
-    active: 'ref4',
+    active: 'ref5',
   },
 ]
 
